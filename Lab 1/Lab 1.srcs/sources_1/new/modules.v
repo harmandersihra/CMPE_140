2057a87537d4af7bb934a76c1aabdf122c077f03
Harmander Sihra <sihraharmander@gmail.com>
2017-08-30 18:43:43 -0700

    fixed forreal

--- Lab 1/Lab 1.srcs/sources_1/new/modules.v
+++ Lab 1/Lab 1.srcs/sources_1/new/modules.v
@@ -1,94 +1,94 @@
 `timescale 1ns / 1ps
 
 module bin2bcd32(
-	input wire [31:0] value,
-	output wire [3:0] dig0,
-	output wire [3:0] dig1,
-	output wire [3:0] dig2,
-	output wire [3:0] dig3,
-	output wire [3:0] dig4,
-	output wire [3:0] dig5,
-	output wire [3:0] dig6,
-	output wire [3:0] dig7
-	);
+    input wire [31:0] value,
+    output wire [3:0] dig0,
+    output wire [3:0] dig1,
+    output wire [3:0] dig2,
+    output wire [3:0] dig3,
+    output wire [3:0] dig4,
+    output wire [3:0] dig5,
+    output wire [3:0] dig6,
+    output wire [3:0] dig7
+    );
 
-	assign dig0 = value % 10;
-	assign dig1 = (value / 10) % 10;
-	assign dig2 = (value / 100) % 10;
-	assign dig3 = (value / 1000) % 10;
-	assign dig4 = (value / 10000) % 10;
-	assign dig5 = (value / 100000) % 10;
-	assign dig6 = (value / 1000000) % 10;
-	assign dig7 = (value / 10000000) % 10;
+    assign dig0 = value % 10;
+    assign dig1 = (value / 10) % 10;
+    assign dig2 = (value / 100) % 10;
+    assign dig3 = (value / 1000) % 10;
+    assign dig4 = (value / 10000) % 10;
+    assign dig5 = (value / 100000) % 10;
+    assign dig6 = (value / 1000000) % 10;
+    assign dig7 = (value / 10000000) % 10;
 
 endmodule
 
 // Multiplier module, default bus width: 32 bits
 module multiplier #(parameter bus_width = 32)(
-	// Two data inputs
-	input wire [bus_width - 1:0] A, B,
+    // Two data inputs
+    input wire [bus_width - 1:0] A, B,
 
-	// Multiplier data output
-	output wire [bus_width - 1:0] out
-	);
+    // Multiplier data output
+    output wire [bus_width - 1:0] out
+    );
 
-	assign out = A * B;
+    assign out = A * B;
 
 endmodule
 
 // Simple 2-input multiplexor, default bus width: 32 bits
 module mux2 #(parameter bus_width = 32)(
-	// Two data inputs
-	input wire [bus_width - 1:0] A, B,
+    // Two data inputs
+    input wire [bus_width - 1:0] A, B,
 
-	// Select line input
-	input wire select,
+    // Select line input
+    input wire select,
 
-	// Data output
-	output wire [bus_width - 1:0] out
-	);
+    // Data output
+    output wire [bus_width - 1:0] out
+    );
 
-	assign out = select ? A : B;
+    assign out = select ? A : B;
 
 endmodule
 
 // Counter module with load and enable
 module cnt_down #(parameter width = 32)(
-	// Control signal inputs
-	input wire load, enable, clk,
-	// Data input
-	input wire [width - 1:0] D,
+    // Control signal inputs
+    input wire load, enable, clk,
+    // Data input
+    input wire [width - 1:0] D,
 
-	// Data output
-	output wire [width - 1:0] Q
-	);
+    // Data output
+    output wire [width - 1:0] Q
+    );
 
-	// Internal register to hold data
-	reg [width - 1:0] mem;
+    // Internal register to hold data
+    reg [width - 1:0] mem;
 
-	// Assign output to internal register
-	assign Q = mem;
+    // Assign output to internal register
+    assign Q = mem;
 
-	always@(posedge clk)
-	begin
-		// If load is enabled, load data input into internal register
-		if (load) mem = D;
+    always@(posedge clk)
+    begin
+        // If load is enabled, load data input into internal register
+        if (load) mem = D;
 
-		// Count down if enable is enabled
-		if (enable) mem = mem - 4'b0001;
-	end
+        // Count down if enable is enabled
+        if (enable) mem = mem - 4'b0001;
+    end
 endmodule
 
 // Compare module
 module compare #(parameter width = 32)(
-	// Two Data inputs
-	input wire [width - 1:0] A, B,
+    // Two Data inputs
+    input wire [width - 1:0] A, B,
 
-	// Data output
-	output wire out
-	);
+    // Data output
+    output wire out
+    );
 
-	assign out = A > B ? 1 : 0;
+    assign out = A > B ? 1 : 0;
 
 endmodule
 
@@ -100,10 +100,10 @@ module register #(parameter width = 32)(
 
     always@(posedge clk)
     begin
-    	if (loadreg)
-    	begin
-    		Q = D;
-    	end
+        if (loadreg)
+        begin
+            Q = D;
+        end
     end
 
 endmodule
