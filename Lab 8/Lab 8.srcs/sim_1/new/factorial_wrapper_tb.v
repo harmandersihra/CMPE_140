0fa877afaf38c651e0f195d040d6b790dd42088b
Harmander Sihra <sihraharmander@gmail.com>
2017-11-15 21:45:52 -0800

    Added cycle counting to the factorial wrapper's test bench

--- Lab 8/Lab 8.srcs/sim_1/new/factorial_wrapper_tb.v
+++ Lab 8/Lab 8.srcs/sim_1/new/factorial_wrapper_tb.v
@@ -6,6 +6,7 @@ module factorial_wrapper_tb();
     reg [3:0] data;
 
     wire [31:0] rd;
+    integer i, count;
 
     factorial_wrapper DUT(.we(we), .clk(clk), .rst(rst), .address(address), .data(data), .rd(rd));
 
@@ -28,10 +29,7 @@ module factorial_wrapper_tb();
         // Watching done bit and checking if it turns 1
         address = 2;
         tik_tok;
-        while(rd != 1)
-        begin
-            tik_tok;
-        end
+        while(!rd) tik_tok;
 
         // Put result out of mux
         address = 3;
@@ -47,11 +45,25 @@ module factorial_wrapper_tb();
         tik_tok;
         address = 2;
         tik_tok;
-        while(!rd)
-        begin
-            tik_tok;
-        end
+        while(!rd) tik_tok;
         if(rd != 2) $display("Error flag not set when n is too large.");
+        tik_tok;
+
+        // Getting cycles needed for factorials 1-12
+        for(i = 0; i < 13; i = i + 1)
+        begin
+            count = 0;
+            address = 0;
+            data = i;
+            tik_tok;
+            address = 1;
+            data = 1;
+            tik_tok;
+            address = 2;
+            tik_tok;
+            while(!rd) tik_tok;
+            $display("%d factorial took %d cycles.", i, count);
+        end
 
         $display("Test bench finished");
         $finish;
@@ -59,6 +71,7 @@ module factorial_wrapper_tb();
 
     task tik_tok;
     begin
+        count = count + 1;
         clk = 0;
         #2;
         clk = 1;
