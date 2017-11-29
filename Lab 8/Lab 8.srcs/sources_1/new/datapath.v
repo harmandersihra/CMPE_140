59477314b4fd312a39afe8fc2d77f60427ece46a
Harmander Sihra <sihraharmander@gmail.com>
2017-11-28 21:31:47 -0800

    Lab 8 - Added E reg

--- Lab 8/Lab 8.srcs/sources_1/new/datapath.v
+++ Lab 8/Lab 8.srcs/sources_1/new/datapath.v
@@ -1,7 +1,7 @@
 module datapath
-(input clk, mult_en, jump_reg, rst, pc_src, jump, we_reg, alu_src, dm2reg, [2:0] alu_ctrl, [1:0] reg_dst, mult_sel, [4:0] ra3, [31:0] instr, rd_dm, output zero, [31:0] pc_current, alu_out, wd_dm, rd3);
+(input clk, mult_en, jump_reg, rst, pc_src, jump, we_reg, alu_src, dm2reg, [2:0] alu_ctrl, [1:0] reg_dst, mult_sel, [4:0] ra3, [31:0] instr, rd_dm, output zero, [31:0] pc_current, alu_out, wd_dmM, rd3);
     wire [4:0]  rf_wa;
-    wire [31:0] pc_plus4, pc_plus4D, pc_pre, pc_next, sext_imm, ba, bta, jta, alu_pa, alu_pb, wd_rf, instrD;
+    wire [31:0] pc_plus4, pc_plus4D, pc_plus4E, pc_pre, pc_next, sext_imm, ba, bta, jta, alu_pa, alu_paE, alu_pb, wd_rf, instrD, instrE, wd_dmE;
     wire [63:0] mult_out;
     wire [31:0] lo_out, hi_out, mult_mux_out, jr_mux_out;
     assign ba = {sext_imm[29:0], 2'b00};
@@ -16,23 +16,24 @@ module datapath
 
     // --- RF Logic --- //
     //mux2 #(5)  rf_wa_mux  (reg_dst, instr[20:16], instr[15:11], rf_wa);
-    mux4 #(5)  rf_wa_mux  (reg_dst, instr[20:16], instr[15:11], 31, 0, rf_wa);
-    regfile    rf         (clk, we_reg, instr[25:21], instr[20:16], ra3, rf_wa, mult_mux_out, alu_pa, wd_dm, rd3);
+    mux4 #(5)  rf_wa_mux  (reg_dst, instrE[20:16], instrE[15:11], 31, 0, rf_wa);
+    regfile    rf         (clk, we_reg, instrD[25:21], instrD[20:16], ra3, rf_wa, mult_mux_out, alu_pa, wd_dm, rd3);
     signext    se         (instr[15:0], sext_imm);
 
     // --- ALU Logic --- //
-    mux2 #(32) alu_pb_mux (alu_src, wd_dm, sext_imm, alu_pb);
-    alu        alu        (alu_ctrl, alu_pa, alu_pb, zero, alu_out);
+    mux2 #(32) alu_pb_mux (alu_src, wd_dmE, sext_imm, alu_pb);
+    alu        alu        (alu_ctrl, alu_paE, alu_pb, zero, alu_out);
 
     // --- MEM Logic --- //
     mux2 #(32) rf_wd_mux  (dm2reg, alu_out, rd_dm, wd_rf);
 
     // --- MULTU Logic --- //
-    mult multu (alu_pa, wd_dm, mult_out);
+    mult multu (alu_paE, wd_dmE, mult_out);
     dreg2 lo (clk, rst, mult_en, mult_out[31:0], lo_out);
     dreg2 hi (clk, rst, mult_en, mult_out[63:32], hi_out);
     mux4 #(32) mult_mux(mult_sel, pc_plus4D, wd_rf, lo_out, hi_out, mult_mux_out);
 
     // --- Pipeline registers --- //
-    register #(64) d_reg(.D({instr, pc_plus4}), .Q({instrD, pc_plus4D}), .clk(clk), .loadreg(1'b1));
+    register #(64)  d_reg(.D({instr, pc_plus4}), .Q({instrD, pc_plus4D}), .clk(clk), .loadreg(1'b1));
+    register #(117) e_reg(.D({alu_pa, wd_dm, instrD[20:0], pc_plus4D}), .Q({alu_paE, wd_dmE, instrE[20:0], pc_plus4E}), .clk(clk), .loadreg(1'b1));
 endmodule
