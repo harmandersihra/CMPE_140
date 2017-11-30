b5fa9ca223fc130acefdecf01f94189b05f1e2d2
Harmander Sihra <sihraharmander@gmail.com>
2017-11-29 21:54:35 -0800

    Lab 8 - shit's still fucked bois

--- Lab 8/Lab 8.srcs/sources_1/new/soc_single_MIPS_tb.v
+++ Lab 8/Lab 8.srcs/sources_1/new/soc_single_MIPS_tb.v
@@ -4,9 +4,9 @@ module soc_single_MIPS_tb;
     reg clk, rst;
     reg [31:0] gpi1, gpi2, count;
     wire [31:0] gpo1, gpo2;
-    
+
     soc_single_MIPS DUT(clk, rst, gpi1, gpi2, gpo1, gpo2);
-    
+
     initial begin
     count=0;
     rest;
@@ -14,43 +14,44 @@ module soc_single_MIPS_tb;
         begin
             gpi1=count;
             gpi2=count;
-            tik_tok;
-            if(DUT.fac.done==1||DUT.fac.error==1)
+            while(DUT.fac.done != 1 && DUT.fac.error != 1)
             begin
-                case(count)
-                32'd0: if(DUT.fac.prod!=1)
-                    fail;
-                32'd1: if(DUT.fac.prod!=1)
-                    fail;
-                32'd2: if(DUT.fac.prod!=2)
-                    fail;
-                32'd3: if(DUT.fac.prod!=6)
-                    fail; 
-                32'd4: if(DUT.fac.prod!=24)
-                    fail; 
-                32'd5: if(DUT.fac.prod!=120)
-                    fail;
-                32'd6: if(DUT.fac.prod!=720)
-                    fail; 
-                32'd7: if(DUT.fac.prod!=5040)
-                    fail; 
-                32'd8: if(DUT.fac.prod!=40320)
-                    fail; 
-                32'd9: if(DUT.fac.prod!=362880)
-                    fail; 
-                32'd10: if(DUT.fac.prod!=3628800)
-                    fail; 
-                32'd11: if(DUT.fac.prod!=39916800)
-                    fail;     
-                32'd12: if(DUT.fac.prod!=479001600)
-                    fail;   
-                32'd13: if(DUT.fac.error==1)
-                    pass; 
-                default:
-                    fail;                                  
-                endcase
-                count=count+1;
+                tik_tok;
             end
+            case(count)
+            32'd0: if(DUT.fac.prod!=1)
+                fail;
+            32'd1: if(DUT.fac.prod!=1)
+                fail;
+            32'd2: if(DUT.fac.prod!=2)
+                fail;
+            32'd3: if(DUT.fac.prod!=6)
+                fail;
+            32'd4: if(DUT.fac.prod!=24)
+                fail;
+            32'd5: if(DUT.fac.prod!=120)
+                fail;
+            32'd6: if(DUT.fac.prod!=720)
+                fail;
+            32'd7: if(DUT.fac.prod!=5040)
+                fail;
+            32'd8: if(DUT.fac.prod!=40320)
+                fail;
+            32'd9: if(DUT.fac.prod!=362880)
+                fail;
+            32'd10: if(DUT.fac.prod!=3628800)
+                fail;
+            32'd11: if(DUT.fac.prod!=39916800)
+                fail;
+            32'd12: if(DUT.fac.prod!=479001600)
+                fail;
+            32'd13: if(DUT.fac.error==1)
+                pass;
+            default:
+                fail;
+            endcase
+            count = count + 1;
+            tik_tok;
         end
     end
     task tik_tok;
@@ -62,7 +63,7 @@ module soc_single_MIPS_tb;
         clk = 0;
     end
     endtask
-    
+
     task rest;
     begin
         rst=1;
